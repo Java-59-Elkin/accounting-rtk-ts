2ee9f841f0b8afb367da42fc47f2169b462bbfe8
Mikhail <elkinms85@gmail.com>
2025-03-06 00:15:41 +0200

    add Route dependence on token

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css'
 import Guest from './components/Guest'
 import Profile from "./components/Profile";
+import {Route, Routes, Navigate} from "react-router";
 
 function App() {
+    const token = 't';
 
-  return (
-      <Profile/>
-  )
+    return (
+        <>
+            <Routes>
+                {token ?
+                    (<>
+                        (<Route path='/profile' element={<Profile/>}/>)
+                        (<Route path='/' element={<Navigate to='/profile'/>}/>)
+                    </>) :
+                    (<>
+                        <Route path='/' element={<Guest/>}/>
+                        <Route path='/profile' element={<Navigate to='/'/>}/>
+                    </>)
+                }
+            </Routes>
+        </>
+    )
 }
 
 export default App
